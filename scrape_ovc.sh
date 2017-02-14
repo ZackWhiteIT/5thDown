ccbae5a15d87e287ebd2c60aa6920b6c8a2ac1b0
Zack White <zackwhiteal@gmail.com>
2017-02-14 16:41:17 -0500

    Add OVC data back to 2008

--- scrape_ovc.sh
+++ scrape_ovc.sh
@@ -1,11 +1,21 @@
 #! /bin/bash
 
-INPUT=data/ovc.csv
-mkdir data/ovc
+input=data/ovc.csv
+years=( 2008 2009 2010 2011 2012 2013 2014 2015 2016 )
 
-[ ! -f $INPUT ] && { echo "$INPUT file not found"; exit 99; }
+for year in "${years[@]}"
+do
+  if [ ! -d "data/$year/ovc" ]; then
+    mkdir -p data/$year/ovc
+  fi
+done
+
+
+[ ! -f $input ] && { echo "$input file not found"; exit 99; }
 while IFS=, read source destination
 do
-  fifthdown scrape $source > $destination
-  echo $destination created.
-done < $INPUT
+  if [ ! -f "$destination" ]; then
+    fifthdown scrape $source > $destination
+    echo $destination created.
+  fi
+done < $input
